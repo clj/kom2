eff341d52b49bd3973eee7f3130f91402a04e182
Christian Jacobsen <me@cljacobsen.com>
2023-05-16 23:41:17 +0200

    Add missing package script

---
+++ package.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+src=$1
+version=$2
+
+cd $src
+for lib in kom-*; do
+    ext=${lib##*.}
+    filename=${lib%%.*}
+    filename=${filename/darwin/macos}
+    zip=${filename/kom/kicad-odbc-middleware2-$version}.zip
+    cp $lib kom.$ext
+    zip $zip kom.$ext
+    rm kom.$ext
+done
