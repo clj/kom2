75671a0fb643eddb152528f58024fc238b2efd51
Christian Jacobsen <me@cljacobsen.com>
2023-06-01 21:58:29 +0200

    Use a matrix build instead of xgo to build packages

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 
 case $(uname -s) in
     Darwin*)   ext="dylib";;
+    MINGW64*)  ext="dll";;
     *)         ext="so";;
 esac
 
 set -x
 
-go build -buildmode=c-shared -o kom2.$ext
+go build -buildmode=c-shared -o kom2.$ext "$@"
