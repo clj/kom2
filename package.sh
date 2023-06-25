f61c7718e23dffc0239088365cc6b47ff6162953
Christian Jacobsen <me@cljacobsen.com>
2023-06-26 00:26:47 +0200

    Fix bash conditional stmt

--- package.sh
+++ package.sh
@@ -10,7 +10,7 @@ for lib in kom2-*; do
     filename=${filename/darwin/macos}
     zip=${filename/kom2/kicad-odbc-middleware2-$version}.zip
     cp $lib kom2.$ext
-    if "$ext" == "dll"; then
+    if [ "$ext" = "dll" ]; then
         zip $zip kom2.$ext inst.*
     else
         zip $zip kom2.$ext
