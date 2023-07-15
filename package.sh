834248822a63193b1fcd8516acba3a2f36e22a3f
Christian Jacobsen <me@cljacobsen.com>
2023-07-15 15:09:58 +0200

    Add some output to the packaging script

--- package.sh
+++ package.sh
@@ -5,17 +5,19 @@ version=$2
 
 cd $src
 
+echoexec() { echo "\$ $@" ; "$@" ; }
+
 for lib in kom2-*{.so,.dylib}; do
     ext=${lib##*.}
     filename=${lib%%.*}
     filename=${filename/darwin/macos}
     zip=${filename/kom2/kicad-odbc-middleware2-$version}.zip
     cp $lib kom2.$ext
-    zip $zip kom2.$ext
+    echoexec zip $zip kom2.$ext
     rm kom2.$ext
 done
 
 for installer in kom2-windows-*; do
     dst=${installer/kom2/kicad-odbc-middleware2-$version}
-    cp $installer $dst
+    echoexec cp $installer $dst
 done
