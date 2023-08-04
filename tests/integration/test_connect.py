e1fa7716d1f9ced147eeb1d8bf11859187958659
Christian Jacobsen <me@cljacobsen.com>
2023-08-04 22:56:20 +0200

    Use pypyodbc instead of pyodbc

--- tests/integration/test_connect.py
+++ tests/integration/test_connect.py
@@ -1,7 +1,7 @@
 import json
 import sys
 
-import pyodbc
+import pypyodbc
 import pytest
 
 
@@ -10,24 +10,24 @@ if sys.platform.startswith("win"):
 
 
 def test_connect_without_server(driver_name):
-    with pytest.raises(pyodbc.OperationalError) as exception:
-        pyodbc.connect(f"Driver={driver_name}")
+    with pytest.raises(pypyodbc.DatabaseError) as exception:
+        pypyodbc.connect(f"Driver={driver_name}")
 
     assert exception.value.args[0] == "08001"
     assert "No Server specified" in exception.value.args[1]
 
 
 def test_connect_no_credentials(driver_name):
-    with pytest.raises(pyodbc.OperationalError) as exception:
-        pyodbc.connect(f"Driver={driver_name};server=asdf")
+    with pytest.raises(pypyodbc.DatabaseError) as exception:
+        pypyodbc.connect(f"Driver={driver_name};server=asdf")
 
     assert exception.value.args[0] == "08001"
     assert "No APIToken or Username+Password specified" in exception.value.args[1]
 
 
 def test_connect_invalid_server(driver_name):
-    with pytest.raises(pyodbc.OperationalError) as exception:
-        pyodbc.connect(f"Driver={driver_name};server=asdf://asdf;apitoken=asdf")
+    with pytest.raises(pypyodbc.DatabaseError) as exception:
+        pypyodbc.connect(f"Driver={driver_name};server=asdf://asdf;apitoken=asdf")
 
     assert exception.value.args[0] == "08001"
     assert "Error updating category list" in exception.value.args[1]
@@ -35,8 +35,8 @@ def test_connect_invalid_server(driver_name):
 
 def test_connect_no_server(driver_name, port):
     hostname, portnumber = port
-    with pytest.raises(pyodbc.OperationalError) as exception:
-        pyodbc.connect(
+    with pytest.raises(pypyodbc.DatabaseError) as exception:
+        pypyodbc.connect(
             f"Driver={driver_name};server=http://{hostname}:{portnumber};apitoken=asdf;httptimeout=1ms"
         )
 
@@ -46,8 +46,8 @@ def test_connect_no_server(driver_name, port):
 
 def test_connect_log(driver_name, tmp_path):
     logfile = tmp_path / "logfile.log"
-    with pytest.raises(pyodbc.OperationalError) as exception:
-        pyodbc.connect(f"Driver={driver_name};logfile={logfile}")
+    with pytest.raises(pypyodbc.DatabaseError) as exception:
+        pypyodbc.connect(f"Driver={driver_name};logfile={logfile}")
 
     assert exception.value.args[0] == "08001"
     assert "No Server specified" in exception.value.args[1]
@@ -59,8 +59,8 @@ def test_connect_log(driver_name, tmp_path):
 def test_invalid_credentials(driver_name, httpserver):
     server = httpserver.url_for("")
     httpserver.expect_request("/api/user/token").respond_with_data(status=401)
-    with pytest.raises(pyodbc.OperationalError) as exception:
-        pyodbc.connect(
+    with pytest.raises(pypyodbc.DatabaseError) as exception:
+        pypyodbc.connect(
             f"Driver={driver_name};server={server};username=asdf;password=asdf"
         )
 
